2f2d24440996ab5389ee3fea32704bf9108a1709
Ed Ilyin <ed.il@live.com>
2016-05-03 20:54:17 +0300

    switches to pipeline of custom functions

--- EdIlyin.Accounting.psm1
+++ EdIlyin.Accounting.psm1
@@ -1,20 +1,58 @@
+#                                                                     12345678
+function Get-Accounting {
+    ls | Get-AccountingData | echo
+}
 function Get-BankFolder {
     param (
         [Parameter(ValueFromPipeline)][string]$InvocationName
     )
-    $InvocationName -replace '^(.+)\\.+?\.ps1$', '$1\BankExports' `
-      | Write-Output
+    process {
+        $InvocationName -replace '^(.+)\\.+?\.ps1$', '$1\BankExports' `
+          | echo
+    }
 }
 function Get-AccountingData {
     param (
-        [Parameter(ValueFromPipeline)][System.IO.FileInfo]$InputObject
+        [Parameter(ValueFromPipeline)][System.IO.FileInfo]$FileInfo
     )
     process {
-        switch ($InputObject.Extension) {
-            .csv { $InputObject | cat | ConvertFrom-Csv }
+        $FileInfo `
+          | Get-EhiContent `
+          | ConvertFrom-EhiCsvOrXml `
+          | Rename-EhiProperties `
+          | echo
         }
+}
+function Get-EhiContent {
+    param (
+        [Parameter(ValueFromPipeline)][System.IO.FileInfo]$FileInfo
+    )
+    process {
+        [pscustomobject]@{
+            Content = $FileInfo | cat -e UTF8
+            FileInfo = $FileInfo
+        } | echo
     }
 }
-function Get-Accounting {
-    ls | Get-AccountingData
+function ConvertFrom-EhiCsvOrXml {
+    param (
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [System.Object[]]$Content,
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [System.IO.FileInfo]$FileInfo
+    )
+    process {
+        $Content `
+          | ConvertFrom-Csv `
+          | Add-Member -pa FileInfo $FileInfo `
+          | echo
+    }
+}
+function Rename-EhiProperties {
+    param (
+        [Parameter(ValueFromPipeline)]$Transaction
+    )
+    process {
+        $Transaction | echo
+    }
 }
